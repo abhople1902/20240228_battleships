568b0c8268a7e021a8a12eb904abbb2d1b11c6ca
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-03-11 03:18:46 +0530

    Added ship placer page

--- 02_UX/BattleshipProject/angularCode/src/app/maingame/placer/placer.component.spec.ts
+++ 02_UX/BattleshipProject/angularCode/src/app/maingame/placer/placer.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavComponent } from './nav.component';
+import { PlacerComponent } from './placer.component';
 
-describe('NavComponent', () => {
-  let component: NavComponent;
-  let fixture: ComponentFixture<NavComponent>;
+describe('PlacerComponent', () => {
+  let component: PlacerComponent;
+  let fixture: ComponentFixture<PlacerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavComponent]
+      imports: [PlacerComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(NavComponent);
+    fixture = TestBed.createComponent(PlacerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
