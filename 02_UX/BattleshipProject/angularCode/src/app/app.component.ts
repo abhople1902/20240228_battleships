82f13ddc600016028fbf7330464939442686ebe2
muditmalviya <malviyamudit7@gmail.com>
2024-03-11 01:34:17 +0530

    Added the main game page

--- 02_UX/BattleshipProject/angularCode/src/app/app.component.ts
+++ 02_UX/BattleshipProject/angularCode/src/app/app.component.ts
@@ -5,12 +5,14 @@ import { NavbarComponent } from './auth/navbar/navbar.component';
 import { NavBarComponent } from './mainpage/nav-bar/nav-bar.component';
 import { ContentComponent } from './mainpage/content/content.component';
 import { BodyComponent } from './mainpage/body/body.component';
+import { NavComponent } from './maingame/nav/nav.component';
+import { GridComponent } from './maingame/grid/grid.component';
 // import { NavBarComponent } from './mainpage/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent, ContentComponent, BodyComponent,NavbarComponent,MainBodyComponent],
+  imports: [RouterOutlet,NavBarComponent, ContentComponent, BodyComponent,NavbarComponent,MainBodyComponent, NavComponent, GridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
