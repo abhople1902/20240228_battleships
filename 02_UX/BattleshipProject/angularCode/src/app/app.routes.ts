607a96f927d38e81655d18aaee9fe2897424a4f5
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2024-03-11 12:05:51 +0530

    not found page added

--- 02_UX/BattleshipProject/angularCode/src/app/app.routes.ts
+++ 02_UX/BattleshipProject/angularCode/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { SigninComponent } from './auth/input-login/signin/signin.component';
 import { RegiterComponent } from './auth/input-register/regiter/regiter.component';
 import { GridComponent } from './maingame/grid/grid.component';
 import { PlacerComponent } from './maingame/placer/placer.component';
+import { NotfoundpageComponent } from './notfound/notfoundpage/notfoundpage.component';
 
 export const routes: Routes = [
 // Routing part for the SigninComponent AND RegiterComponent
     {path: '', component: SigninComponent},
     {path: 'register', component: RegiterComponent},
     {path: 'gamestart', component: GridComponent},
-    {path: 'shipplacer', component: PlacerComponent}
+    {path: 'shipplacer', component: PlacerComponent},
+    { path: '**', component: NotfoundpageComponent }
 ];
