07ae9ea77c81b431e18da0ffc7fcbe830456337e
D46-keshavkhandelwal <122668134+keshav-09@users.noreply.github.com>
2024-03-12 17:57:51 +0530

    Added Swagger and Logic for checkIndex and shipPlace

--- 06_new_backend/controllers/gameController.js
+++ 06_new_backend/controllers/gameController.js
@@ -46,6 +46,7 @@ const Game = require("../models/Game");
  *         description: Error message
  */
 async function createGame(req, res) {
+
   try {
     const game = new Game(req.body);
     const savedGame = await game.save();
@@ -54,19 +55,133 @@ async function createGame(req, res) {
     res.status(500).json({ message: "Internal server error" });
   }
 }
+/** Controller for checkIndex */
+async function checkShipPlacement(req, res) {
+  try {
+    const { gameId, playertype, position } = req.body;
 
-/** Controller for updating product */
-async function updateProduct(req, res) {}
+    const gaming = await Game.findOne({ gameId: gameId });
+    const typeOfPlayer = playertype;
+
+    if (!typeOfPlayer) {
+      return res.status(404).json({ success: false, message: 'Player not found' });
+    }
+
+    const { x, y } = position;
+
+    if (typeOfPlayer === 'Human') {
+      const shipPlacements2 = gaming.placementsPlayer2;
+
+      (shipPlacements2 || []).forEach(item => {
+        const xValues = item.shipPlacements.map(ship => ship.x);
+        if (xValues.includes(x)) {
+          console.log(`pos1 matches an x value in placementsPlayer2`);
+        } else {
+          console.log(`pos1 does not match an x value in placementsPlayer2`);
+        }
+        const yValues = item.shipPlacements.map(ship => ship.y);
+        if (yValues.includes(y)) {
+          console.log(`pos2 matches a y value in placementsPlayer2`);
+        } else {
+          console.log(`pos2 does not match a y value in placementsPlayer2`);
+        }
+      });
+    } else {
+      const shipPlacements1 = gaming.placementsPlayer1;
+
+      (shipPlacements1 || []).forEach(item => {
+        const xValues = item.shipPlacements.map(ship => ship.x);
+        if (xValues.includes(x)) {
+          console.log(`pos1 matches an x value in placementsPlayer1`);
+        } else {
+          console.log(`pos1 does not match an x value in placementsPlayer1`);
+        }
+        const yValues = item.shipPlacements.map(ship => ship.y);
+        if (yValues.includes(y)) {
+          console.log(`pos2 matches a y value in placementsPlayer1`);
+        } else {
+          console.log(`pos2 does not match a y value in placementsPlayer1`);
+        }
+      });
+    }
+
+    await gaming.save();
+
+    res.json({
+      success: true,
+      message: 'Ships placements checked successfully.'
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+}
+
+/** Controller for  saveShipPlacements */
+async function  saveShipPlacements(req, res) { 
+  try {
+    const { gameId, playertype, playerID, position, shipType } = req.body;
+
+    // Check if the game exists
+    const gaming = await Game.findOne({ gameId: gameId });
+
+    if (!gaming) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Game not found" });
+    }
+
+    // Validating the input
+    if (
+      !playerValidator(playerID) ||
+      !positionValidator(position) ||
+      !shipValidator(shipType) ||
+      !playerTypeValidator(playertype)
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid input" });
+    }
+
+    // Saving the ship placements
+    const placement = {
+      playerID,
+      shipPlacements: position,
+      shipType,
+    };
+
+    // Human ships correspond to player 1 and
+    // computer ships correspond to player 2.
+    if (playertype === "Human") {
+      gaming.placementsPlayer1.push(placement);
+    } else {
+      gaming.placementsPlayer2.push(placement);
+    }
+
+    await gaming.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Ships placements saved successfully.",
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+}
 
 /** Controller for deleting product */
-async function deleteProduct(req, res) {}
+async function deleteProduct(req, res) { }
 
 /** Controller for getting seller dashboard */
-async function getDashboard(req, res) {}
+async function getDashboard(req, res) { }
 
 module.exports = {
   createGame,
-  updateProduct,
+  saveShipPlacements,
+  checkShipPlacement,
   deleteProduct,
   getDashboard,
 };
